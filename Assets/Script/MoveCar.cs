f57b171d45257637770ea4acbbd92c8e1e30a02a
Rist/Desktop <www10177@gmail.com>
2016-11-22 23:31:16 +0800

    自動產生車子簡易版完成

---
+++ Assets/Script/MoveCar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCar : MonoBehaviour {
+    float Speed = 0.2f;
+    float MaximumXLocation = 48f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Speed, 0, 0));
+        print(transform.position.x);
+        if (transform.position.x > MaximumXLocation)
+            Destroy(this.gameObject);
+
+	}
+}
---
+++ Assets/Script/SpawnCars.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCars : MonoBehaviour {
+    float CarRangeZ=25.71f/2; // Range of spawn cars
+    float CarOffsetY = 1.28f;//Distanve between car center and the plane
+    int CarDestiny = 60;// Every "CarDestiny" frame will spawn one car
+    public GameObject Cars;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.frameCount % CarDestiny == 1) 
+            Instantiate(Cars, transform.position + new Vector3(0, CarOffsetY, Random.Range((int)(-1 * CarRangeZ), (int)CarRangeZ)), transform.rotation);
+    }
+}
