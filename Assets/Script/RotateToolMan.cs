5aeb231c5085d0090e1969106749ffd2f44947d4
oblivion <oblivion8514@gmail.com>
2016-11-28 00:30:25 +0800

    update camerafollow ,rotatetoolman and toolmanPrefab

--- Assets/Script/RotateToolMan.cs
+++ Assets/Script/RotateToolMan.cs
@@ -2,15 +2,14 @@
 using System.Collections;
 
 public class RotateToolMan : MonoBehaviour {
-
+	public float SpeedHor = 1.0f; //水平旋轉(左右轉頭)的速度
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float SpeedHor = 1.0f; //水平旋轉(左右轉頭)的速度
 		this.transform.Rotate (new Vector3(0,Input.GetAxis("Mouse X"),0)*SpeedHor,Space.Self);
+		this.transform.GetChild (3).Rotate (new Vector3 (Input.GetAxis ("Mouse Y"), 0, 0) * SpeedHor, Space.Self);
 	}
 }
