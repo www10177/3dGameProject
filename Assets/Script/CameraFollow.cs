c830c84cf1c1b4818bbf0cdaf805a8302ff0d69e
oblivion <oblivion8514@gmail.com>
2016-11-28 00:30:25 +0800

    update camerafollow ,rotatetoolman and toolmanPrefab

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
-
+	public float SpeedHor = 1.0f; //水平旋轉(左右轉頭)的速度
 	public float CameraHeight;	//攝影機固定在人物上方10m
 	public float CameraDistance;	//攝影機固定在離人物背後水平10m遠
 	public float CameraHorizontalOffset; //攝影機距離肩膀水平距離
@@ -18,6 +18,6 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		this.transform.Rotate(new Vector3 (Input.GetAxis ("Mouse Y"), 0, 0) * 1.0f, Space.Self);
 	}
 }
