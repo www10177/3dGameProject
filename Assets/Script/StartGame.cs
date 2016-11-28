3fa745979c3e50619fe1869f1d1147a7cec4b744
hsinyinfu <peter0930260535@gmail.com>
2016-11-28 22:22:43 +0800

    "完成遊戲開始UI&場景切換"

---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//When Start button be clicked,
+	//the scene changed from start scene to main scene
+	public void StartButtonClick(){
+		Application.LoadLevel ("MainScene");
+	}
+}
