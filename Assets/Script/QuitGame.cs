1b00ea6a1932bc13bc46f4d6d698cb360291e4e3
hsinyinfu <peter0930260535@gmail.com>
2016-11-29 01:44:26 +0800

    新增script，按下esc鍵會跳出視窗，可選擇離開遊戲

---
+++ Assets/Script/QuitGame.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//按esc可以選擇是否離開遊戲
+//應該是因為在編輯者模式，所以按quit後沒辦法離開
+//輸出成可執行檔之後應該就可以正常quit遊戲
+//從網路上幹來的codeㄏㄏ
+//大家可以再討論要不要用
+public class QuitGame : MonoBehaviour
+{
+	// public
+	public int windowWidth = 400;
+	public int windowHight = 150;
+
+	// private
+	Rect windowRect ;
+	int windowSwitch = 0;
+	float alpha = 0;
+
+	void GUIAlphaColor_0_To_1 ()
+	{
+		if (alpha < 1) {
+			alpha += Time.deltaTime;
+			GUI.color = new Color (1, 1, 1, alpha);
+		} 
+	}
+
+	// Init
+	void Awake ()
+	{
+		windowRect = new Rect (
+			(Screen.width - windowWidth) / 2,
+			(Screen.height - windowHight) / 2,
+			windowWidth,
+			windowHight);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown ("escape")) {
+			windowSwitch = 1;
+			alpha = 0; // Init Window Alpha Color
+		}
+	}
+
+	void OnGUI ()
+	{ 
+		if (windowSwitch == 1) {
+			GUIAlphaColor_0_To_1 ();
+			windowRect = GUI.Window (0, windowRect, QuitWindow, "Quit Window");
+		}
+	}
+
+	void QuitWindow (int windowID)
+	{
+		GUI.Label (new Rect (100, 50, 300, 30), "Are you sure you want to quit game ?");
+
+		if (GUI.Button (new Rect (80, 110, 100, 20), "Quit")) {
+			Application.Quit ();
+		} 
+		if (GUI.Button (new Rect (220, 110, 100, 20), "Cancel")) {
+			windowSwitch = 0; 
+		} 
+
+		GUI.DragWindow (); 
+	}
+
+}
