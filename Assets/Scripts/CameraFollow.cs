146319e3b7867e98f3a2820a4b4ff7d426c212e0
Rist/Desktop <www10177@gmail.com>
2016-12-18 03:02:57 +0800

    Added ToolMan Model as Player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
-	public float SpeedVer = 1.0f; //垂直旋轉(上下轉頭、眼)的速度
+	public float SpeedVar = 0.5f; //垂直旋轉(上下轉頭、眼)的速度
 	public float CameraHeight;	//攝影機固定在人物上方10m
 	public float CameraDistance;	//攝影機固定在離人物背後水平10m遠
 	public float CameraHorizontalOffset; //攝影機距離肩膀水平距離
@@ -12,13 +12,14 @@ public class CameraFollow : MonoBehaviour {
 	void Start () {
 		ToolMan = GameObject.Find ("ToolMan").transform;
 		MainCamera = Camera.main.transform;
-		MainCamera.position = new Vector3 (ToolMan.position.x+CameraHorizontalOffset,	//固定camera跟人之間的相對位置
-			ToolMan.position.y+CameraHeight, ToolMan.position.z-CameraDistance);
-	}
+       //固定camera跟人之間的相對位置
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Rotate(new Vector3 (-1*Input.GetAxis ("Mouse Y"), 0, 0) * SpeedVer, Space.Self);
-        this.transform.Rotate(new Vector3(0,    1 * Input.GetAxis("Mouse X"), 0) * SpeedVer, Space.Self);
+        //this.transform.Rotate(new Vector3 (-1*Input.GetAxis ("Mouse Y"), 0, 0) * SpeedVer, Space.Self);
+
+        MainCamera.position = new Vector3(ToolMan.position.x + CameraHorizontalOffset, ToolMan.position.y + CameraHeight, ToolMan.position.z - CameraDistance);
+     //   MainCamera.position = new Vector3(MainCamera.position.x + Input.GetAxis("Mouse X") * SpeedVar, MainCamera.position.y , MainCamera.position.z);
     }
 }
