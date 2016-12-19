28dde07d9e2685c03abb1876c0ba88cb6d84dfab
Rist/Desktop <www10177@gmail.com>
2016-12-20 00:14:38 +0800

    加入TestScene，修改CarMove移動方式，加入兩種車，修改CarSpawn

--- Assets/Scripts/MoveCar.cs
+++ Assets/Scripts/MoveCar.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class MoveCar : MonoBehaviour {
-    public float Speed = 0.2f;
+    public float Speed = 20f;
     float MaximumDistance ; // Max Distance the Car Can Move ( == the Length of the Road ) 
     float OriginalX; //Original point( == CarSpawner)
 	private GameObject CarSpawner ;
@@ -16,7 +16,7 @@ public class MoveCar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector3(Speed, 0, 0));
+        transform.Translate(new Vector3(Speed*Time.deltaTime, 0, 0));
 		float MoveDis = Mathf.Abs(transform.position.x - OriginalX);//the distance car moved
 		if (MoveDis >= MaximumDistance  )
 			Destroy(this.gameObject);
--- Assets/Scripts/SpawnCars.cs
+++ Assets/Scripts/SpawnCars.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class SpawnCars : MonoBehaviour {
     /*Parameters*/
     private int[] TimeRange =  { 50,100 };
+    public int CarCount = 3;
 
 /*Global Variables*/
 
     int CarSpawnTime ;// Every "CarSpawnTime" frame will spawn one car
-	public GameObject Cars;
+	public GameObject[] Cars;
      public float MaximumDistance; // Maximum Distance that car spawned can move
 
 	// Use this for initialization
@@ -16,7 +17,12 @@ public class SpawnCars : MonoBehaviour {
         MaximumDistance = this.transform.parent.GetComponent<Renderer>().bounds.size.x;
         /*Set CarDestiny Seed*/
         CarSpawnTime = Random.Range(TimeRange[0], TimeRange[1]);
+        Cars = new GameObject[CarCount];
+        for (int i = 0; i < CarCount; i++)
+        {
+            Cars[i] = Resources.Load("Cars/Car" + (i + 1)) as GameObject;
 
+        }
     }
 
     // Update is called once per frame
@@ -31,7 +37,7 @@ public class SpawnCars : MonoBehaviour {
 	void  CreateCar()
 	{
 		
-		GameObject NewCar = Instantiate (Cars, transform.position  , transform.rotation) as GameObject;
+		GameObject NewCar = Instantiate (Cars[Random.Range(0,CarCount)], transform.position  , transform.rotation) as GameObject;
 		NewCar.transform.parent = transform;
 	}
    }
