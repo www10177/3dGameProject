8df4142edeaa17f9f61bbf0256fb59ea92dc34fb
oblivion <oblivion8514@gmail.com>
2016-12-24 03:10:04 +0800

    增加phone的UI，目前僅放在testscene，有將該UI加入prefab，待有人可做出在mainscene在地圖上隨機產生一支手機的腳本

---
+++ Assets/Scripts/PhoneController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneController : MonoBehaviour {
+	public Canvas CanvasObject;
+	bool PhoneObtained=false;
+	void Start () {
+		CanvasObject.GetComponent<Canvas> ().enabled = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)&& PhoneObtained == true) {
+			if (CanvasObject.GetComponent<Canvas> ().enabled == true) {
+				CanvasObject.GetComponent<Canvas> ().enabled = false;
+			} else {
+				CanvasObject.GetComponent<Canvas> ().enabled = true;
+			}
+		}
+	}
+	void OnTriggerEnter(Collider collision){
+		if (collision.gameObject.tag == "Cellphone") {
+			PhoneObtained = true;
+			Destroy (collision.gameObject);
+			CanvasObject.GetComponent<Canvas> ().enabled = true;
+		}
+	}
+}
