6a52d0d0d59df0de07d331f35258be79ef6fe711
oblivion <oblivion8514@gmail.com>
2016-12-29 04:29:45 +0800

    UI可以正常顯示，尚未處理金幣生成在建築物內的問題

---
+++ Assets/Scripts/CoinCollisionTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollisionTest : MonoBehaviour {//純測試，希望拿來開發讓金幣自動離開生成物的腳本
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionStay(Collision other)
+	{
+		if (other.gameObject.layer == 8) {
+			print ("Coin stay in "+other.gameObject.name);
+			GetComponent<Collider> ().isTrigger = false;
+			Destroy (GetComponent<Rigidbody>());
+		}
+
+	}
+	void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.layer == 8) {
+			print ("Coin Exit "+other.gameObject.name);
+			GetComponent<Collider> ().isTrigger = true;
+			Destroy (GetComponent<Rigidbody>());
+		}
+	}
+	//void 
+}
--- Assets/Scripts/EscAndQuitGame.cs
+++ Assets/Scripts/EscAndQuitGame.cs
@@ -38,7 +38,7 @@ public class EscAndQuitGame : MonoBehaviour
 	void Update ()
 	{
 		if (Input.GetKeyDown ("escape")) {
-			Time.timeScale = 0;
+			//Time.timeScale = 0;
 			windowSwitch = 1;
 			alpha = 0; // Init Window Alpha Color
 		}
@@ -58,15 +58,15 @@ public class EscAndQuitGame : MonoBehaviour
 
 		if (GUI.Button (new Rect (80, 110, 100, 20), "Quit")) {
 			Application.Quit ();
-			Time.timeScale = 1;
+			//Time.timeScale = 1;
 		} 
 		if (GUI.Button (new Rect (220, 90, 100, 20), "Restart")) {
 			Application.LoadLevel ("MainScene");
-			Time.timeScale = 1;
+			//Time.timeScale = 1;
 		} 
 		if (GUI.Button (new Rect (220, 130, 100, 20), "Cancel")) {
 			windowSwitch = 0; 
-			Time.timeScale = 1;
+			//Time.timeScale = 1;
 		} 
 
 		GUI.DragWindow (); 
