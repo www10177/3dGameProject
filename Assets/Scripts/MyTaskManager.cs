704a34b94ca1f162335429705798adab82043bd0
hsinyinfu <peter0930260535@gmail.com>
2016-12-30 01:47:56 +0800

    任務系統的架構和雛型的script

---
+++ Assets/Scripts/MyTaskManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// 提供给MyTask class的辅助
+public class MyTaskManager : MonoBehaviour 
+{
+
+	//单例
+	static MyTaskManager singleton;
+	public static TaskState CreateTask(IEnumerator c)
+	{
+		//要創造對象，那么創造完後就要return，所以必須有個用來return的class
+		if(singleton == null)
+		{
+			GameObject go = new GameObject("MyTaskManager");
+			singleton = go.AddComponent<MyTaskManager>();
+		}
+		return new TaskState(c);
+	}
+
+
+
+
+	public class TaskState
+	{
+		IEnumerator coroutine; //用来保存傳遞過來的函式
+		public enum State
+		{
+			Running,//正在運行
+			Paused, //暫停
+			Stopped,//停止/放棄
+			Finished,//完成
+		}
+
+		public State state;
+
+		public delegate void FinishedHander(bool isFinished);
+		public event FinishedHander Finished;
+
+		public TaskState(IEnumerator c)
+		{
+			coroutine = c;
+			state = State.Running;
+		}
+
+
+		public void Start()
+		{
+			//改變任務狀態，並且開啟一個listening coroutine
+			state = State.Running;
+			singleton.StartCoroutine(TaskStateMonitor());
+		}
+
+		//與task中的函式相對應
+		public void Pause()
+		{
+			state = State.Paused;
+		}
+		public void Resume()
+		{
+			if (state == State.Paused)
+			{
+				state = State.Running;
+			}
+		}
+		public void Finish()
+		{
+			state = State.Finished;
+		}
+
+		//主要是用来判斷是否完成任務
+		IEnumerator TaskStateMonitor()
+		{
+			yield return null;
+			IEnumerator e = coroutine;
+			while(state == State.Running)
+			{
+				if (state == State.Paused)
+					yield return null;
+				else
+				{
+					//IEnumerator 有current ，movenext，reset
+					//不停地執行new出来的任務，因为new出来的任務是Enumrator，直到不能movenext了
+					if(e !=null && e.MoveNext())
+					{
+						yield return e.Current;
+					}
+					else
+					{
+						state = State.Finished;
+					}
+				}
+			}
+			FinishedHander handler = Finished;
+			if (handler != null)
+				handler(true); //true
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/UseTask.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 使用範例
+public class UseTask : MonoBehaviour
+{
+
+	IEnumerator TaskOne()
+	{
+
+		while (true) 
+		{
+			/*TaskOne要執行的內容*/
+			Debug.Log ("Finding phone");
+			if (CheckTaskOneFinished ()) 
+			{
+				Debug.Log ("TaskOne Finished");
+				break;
+			}
+			yield return null;
+		}
+
+	}
+
+	bool CheckTaskOneFinished()
+	{
+		if (PhoneUIController.PhoneObtained) {
+			return true;
+		} else
+			return false;
+	}
+
+	IEnumerator TaskTwo()
+	{
+
+		while (true)
+		{
+			/*TaskTwo要執行的內容*/
+			Debug.Log ("Finding w_lt");
+			if (CheckTaskTwoFinished ()) 
+			{
+				Debug.Log ("w_lt is a polar bear");
+				break;
+			}
+			yield return null;
+		}
+
+	}
+
+	bool CheckTaskTwoFinished()
+	{
+		if(Time.time >3)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+/*	IEnumerator TaskOne()
+	{
+		while(true)
+		{
+			Debug.Log("TaskOne running");
+			if(CheckTaskOneFinished())
+			{
+				Debug.Log("TaskOne Finished");
+				DealWithTaskOneFinished();
+				break; //跳出後執行e.moveNext()，接著狀態變成finished
+			}
+			yield return null;
+		}
+	}
+
+	/// 用来檢測任務是否完成
+	bool CheckTaskOneFinished()
+	{
+		if(Time.time >3)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	void DealWithTaskOneFinished()
+	{
+		///
+	}
+*/
+
+	void Start()
+	{
+		MyTask task = new MyTask(TaskOne());
+		MyTask task2;
+
+		//用匿名的方式添加TaskOne finished後的處理
+		task.Finished += delegate(bool value)
+		{
+			if(value)
+			{
+				task2 =  new MyTask (TaskTwo (), true);
+			}
+		};
+
+	}
+
+}
