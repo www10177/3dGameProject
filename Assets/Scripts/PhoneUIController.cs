df1c5191d05337b728b6ef668c2f8b1bc1309256
Rist/Desktop <www10177@gmail.com>
2016-12-31 01:26:03 +0800

    加入鹹酥雞攤，修改UI，Map Objects會自動旋轉

--- Assets/Scripts/PhoneUIController.cs
+++ Assets/Scripts/PhoneUIController.cs
@@ -5,21 +5,25 @@ using UnityEngine.UI;
 public class PhoneUIController : MonoBehaviour {
 	public Canvas PhoneUI;
 	public Canvas DefaultUI;
+    public GameObject cellphoneInUI;
 	public static bool PhoneObtained=false;
 	Transform PanelUI;
 	public Text MoneyText;
 	public Text ScoreText;
 	void Start () {
-		PhoneUI.GetComponent<Canvas> ().enabled = false;
-	}
+        PhoneUI.GetComponent<Canvas>().enabled = false;
+        cellphoneInUI.SetActive(false);
+    }
 
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.C)&& PhoneObtained == true) {
 			if (PhoneUI.GetComponent<Canvas> ().enabled == true) {
-				PhoneUI.GetComponent<Canvas> ().enabled = false;
-			} else {
-				PhoneUI.GetComponent<Canvas> ().enabled = true;
-			}
+                PhoneUI.GetComponent<Canvas>().enabled = false;
+                cellphoneInUI.SetActive(false);
+            } else {
+                PhoneUI.GetComponent<Canvas>().enabled = true;
+                cellphoneInUI.SetActive(true);
+            }
 			DefaultUI.GetComponent<Canvas> ().enabled = false;
 		}
 
